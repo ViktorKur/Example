a588487484c598e6875886ed6168953321eba60e
Viktor K <popvik13@mail.ru>
2023-02-10 04:37:21 +0300

    add  Example003_Sum

---
+++ Example003_Summ/Program.cs
@@ -0,0 +1,7 @@
+﻿//Вычисление суммы чисел введеных из терминала
+Console.WriteLine("Введите 1е число?");
+int festNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2е число?");
+int SecNum =  Convert.ToInt32(Console.ReadLine());
+int Summ = festNum+SecNum;
+Console.WriteLine("{0} + {1} = {2}", festNum, SecNum, Summ);
