28dc8515e9b8603958839f86ee90aa4a15b2aa75
Viktor K <popvik13@mail.ru>
2023-02-10 05:13:48 +0300

    Example004  Деление целочисленных и вещественных чисел

---
+++ Example004_Delenie/Program.cs
@@ -0,0 +1,9 @@
+﻿// Деление целочисленных и вещественных чисел
+Console.WriteLine ("Ведите делимое число?");
+int numA = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("Введите делитель?");
+int numB = Convert.ToInt32 (Console.ReadLine());
+int rez = numA/numB;
+Console.WriteLine ("целочисленное деление: {0} / {1} = {2}", numA, numB, rez);
+double rez2 = Convert.ToDouble (numA) / Convert.ToDouble (numB);
+Console.WriteLine ("вещественное деление: {0} / {1} = {2}", numA, numB, rez2);
