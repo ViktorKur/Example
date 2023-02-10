c882468fbcdeb327634061666c109dfc1bd80864
Viktor K <popvik13@mail.ru>
2023-02-10 23:13:06 +0300

    Создал Example005_Username

---
+++ Example005_UserName/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа приветствия взависимости от имени пользователя
+Console.WriteLine("Введите Ваше имя? ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+  Console.WriteLine ("Ура, это же Маша! Пять рублей и наша!)");
+}
+else
+{
+  Console.WriteLine("Привет, {0}!", username);
+}
